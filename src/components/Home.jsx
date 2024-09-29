0ef9e1be9ea11082f44b55775da35c86ee3ab0b5
Indranil Paul <indranil.paul04@gmail.com>
2024-09-29 18:08:27 +0530

    md

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState ,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Sidebar from './Sidebar';
 import Navbar from './Navbar';
 import Content from './Content';
@@ -15,7 +15,7 @@ const Home = () => {
             <div className="flex min-h-screen">
                 <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
 
-                <div className="flex-1 flex flex-col bg-[#172b4d] ">
+                <div className="flex-1 flex flex-col bg-[#172b4d] h-full relative">
                     <Navbar setIsOpen={setIsOpen} />
                     <Content />
                     <button className=" transition-all size-12 hover:size-14 bg-slate-700 m-2.5 absolute right-0 bottom-1 rounded-full text-gray-300 text-xs "> Need help?</button>
