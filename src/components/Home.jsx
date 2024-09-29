0363885ec25d16068628c71ab325f888028dde8a
Indranil Paul <indranil.paul04@gmail.com>
2024-09-29 18:54:43 +0530

    ..

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,10 +15,13 @@ const Home = () => {
             <div className="flex min-h-screen">
                 <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
 
-                <div className="flex-1 flex flex-col bg-[#172b4d] h-screen relative">
+                <div className="flex-1 flex flex-col bg-[#172b4d] relative">
                     <Navbar setIsOpen={setIsOpen} />
                     <Content />
-                    <button className=" transition-all size-12 hover:size-14 bg-slate-700 m-2.5 absolute right-0 bottom-1 rounded-full text-gray-300 text-xs "> Need help?</button>
+                    <button className="fixed right-2 bottom-3 transition-transform transform hover:scale-110 bg-slate-700 rounded-full text-gray-300 text-xs p-2 size-12 rounded-full">
+                        Need help?
+                    </button>
+
 
                 </div>
             </div>
